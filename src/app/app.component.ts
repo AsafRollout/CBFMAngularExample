f67f57becc31b66584bdb3596c3b7cbb6fba65f0
fufu <asaf@rollout.io>
2022-10-26 15:20:19 +0300

    adding CB FM

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as Rox from 'rox-browser'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  someFlagValue=Rox.dynamicApi.value('someFlag', 'default')
+
+  constructor() {
+    Rox.setup('6357e4c221c6ca36cc89fb54', { configurationFetchedHandler: (res) => {
+      if (res.hasChanges) {
+        this.reloadView()
+      }
+    }})
+  }
+
+  reloadView() {
+    this.someFlagValue=Rox.dynamicApi.value('someFlag', 'default')
+  }
 }
